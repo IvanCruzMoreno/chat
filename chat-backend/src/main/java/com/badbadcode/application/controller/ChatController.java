903a8f66e4d8ed17ef4db2028a6ac8ec3400f3c6
ivan <ivanmoreno405@gmail.com>
2021-01-04 17:58:21 -0600

    i implemented if it's a message or a new user

--- chat-backend/src/main/java/com/badbadcode/application/controller/ChatController.java
+++ chat-backend/src/main/java/com/badbadcode/application/controller/ChatController.java
@@ -1,7 +1,10 @@
 package com.badbadcode.application.controller;
 
 import java.util.Date;
+import java.util.Random;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Controller;
@@ -10,12 +13,23 @@ import com.badbadcode.application.models.documents.Mensaje;
 
 @Controller
 public class ChatController {
-
+	
+	private Logger logger = LoggerFactory.getLogger(ChatController.class);
+	
+	private String colores[] = {"red", "green", "blue", "magenta", "purple", "orange"};
+	
 	@MessageMapping("/mensaje")
 	@SendTo("/chat/mensaje")
 	public Mensaje recibeMensaje(Mensaje mensaje) {
 		mensaje.setFecha(new Date().getTime());
-		mensaje.setTexto("Recibido por el broker: ".concat(mensaje.getTexto()));
+
+		if(mensaje.getTipo().equals("NUEVO_USUARIO")) {
+			mensaje.setColor(colores[new Random().nextInt(colores.length)]);
+			mensaje.setTexto("nuevo usuario");
+		}
+		logger.info(mensaje.getTexto());
+		logger.info(mensaje.getUsername());
+		logger.info(mensaje.getTipo());
 		return mensaje;
 	}
 }
--- chat-backend/src/main/java/com/badbadcode/application/models/documents/Mensaje.java
+++ chat-backend/src/main/java/com/badbadcode/application/models/documents/Mensaje.java
@@ -8,7 +8,9 @@ public class Mensaje implements Serializable{
 	
 	private String texto;
 	private Long fecha;
-	
+	private String username;
+	private String tipo;
+	private String color;
 	
 	public String getTexto() {
 		return texto;
@@ -22,5 +24,24 @@ public class Mensaje implements Serializable{
 	public void setFecha(Long fecha) {
 		this.fecha = fecha;
 	}
+	public String getUsername() {
+		return username;
+	}
+	public String getTipo() {
+		return tipo;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public void setTipo(String tipo) {
+		this.tipo = tipo;
+	}
+	public String getColor() {
+		return color;
+	}
+	public void setColor(String color) {
+		this.color = color;
+	}
+	
 	
 }
